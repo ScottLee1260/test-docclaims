1b96dd9694c317a00a2fa625f7aaa0931ac3a6d1
Scott E. Lee <ScottLee@pobox.com>
2016-01-26 19:00:30 -0500

    minor fixes

--- Test/DocClaims.pm
+++ Test/DocClaims.pm
@@ -31,40 +31,43 @@ sub doc_claims {
     while (1) {
         $test_line = $test->current_line;
         $doc_line  = $doc->current_line;
-#warn "??? T: ", $test_line->text, "\n" if $test_line;
-#warn "???    has_pod ", $test_line->has_pod, "\n" if $test_line;
-#warn "???    is_pod ", $test_line->is_pod, "\n" if $test_line;
-#warn "???    lnum ", $test_line->lnum, "\n" if $test_line;
-#warn "??? D: ", $doc_line->text, "\n" if $doc_line;
-#warn "???    has_pod ", $doc_line->has_pod, "\n" if $doc_line;
-#warn "???    is_pod ", $doc_line->is_pod, "\n" if $doc_line;
-#warn "???    lnum ", $doc_line->lnum, "\n" if $doc_line;
-        if ( $test_line && $test_line->has_pod && !$test_line->is_pod ) {
-            push @skipped_code, $test_line;
+
+        # Skip over the line if it is blank or is a non-POD line in a file
+        # that supports POD.
+        if ( $test_line &&
+                ( ( $test_line->has_pod && !$test_line->is_pod ) ||
+                $test_line->text =~ /^\s*$/ ) )
+        {
+            push @skipped_code, $test_line if $test_line->text =~ /\S/;
             $test->advance_line;
             $test_line = $test->current_line;
-#warn "??? test not pod\n";
             redo;
         }
-        if ( $doc_line && $doc_line->has_pod && !$doc_line->is_pod ) {
+        if ( $doc_line &&
+                ( ( $doc_line->has_pod && !$doc_line->is_pod ) ||
+                $doc_line->text =~ /^\s*$/ ) )
+        {
             $doc->advance_line;
             $doc_line = $doc->current_line;
-#warn "??? doc not pod\n";
             redo;
         }
+
         last if $test->is_eof || $doc->is_eof;
         next if $test_line->text eq $doc_line->text;
-        # ???
-
-        return _diff_error( $test_line, $doc_line, $name );
+        my $tb = Test::DocClaims->builder;
+        my $fail = $tb->ok( 0, $name );
+        _diff_error( $test_line, $doc_line, $name );
+        return $fail;
     } continue {
         $test->advance_line;
         $doc->advance_line;
         @skipped_code = ();
-#warn "??? advance_line\n";
     }
     if ( !$test->is_eof || !$doc->is_eof ) {
-        return _diff_error( $test->current_line, $doc->current_line, $name );
+        my $tb = Test::DocClaims->builder;
+        my $fail = $tb->ok( 0, $name );
+        _diff_error( $test->current_line, $doc->current_line, $name );
+        return $fail;
     } else {
         my $tb = Test::DocClaims->builder;
         return $tb->ok( 1, $name );
@@ -74,22 +77,19 @@ sub doc_claims {
 sub _diff_error {
     my ( $test_line, $doc_line, $name ) = @_;
     my @error;
-    my $prefix = "   got";
+    my $prefix = "     got";
     foreach my $line ( $test_line, $doc_line ) {
         if ( ref $line ) {
-            my $text = $line->orig;
-            $text =~ s/\s+$//;
+            my $text = $line->text;
             push @error, "$prefix: '$text'";
             push @error, "at " . $line->path . " line " . $line->lnum;
         } else {
             push @error, "missing";
         }
-        $prefix = "expect";
+        $prefix = "expected";
     }
     my $tb = Test::DocClaims->builder;
-    my $fail = $tb->ok( 0, $name );
     $tb->diag( map { "    $_\n" } @error );
-    return $fail;
 }
 
 1;
